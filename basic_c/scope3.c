f2f59f2851dee6fba0bb2302737fd9417d436282
Kyohei Oka <0622okakyo@gmail.com>
2018-12-05 18:30:54 +0900

    '練習中'

---
+++ basic_c/scope3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int j=42;
+
+void func3(){
+	int i=11,j=999;
+	printf("\t\t\t[func3“à] i@0x%o8x =%d\n",&i,i);
+	printf("\t\t\t[func3“à] j@0x%o8x =%d\n",&j,j);
+}
+
+void func2(){
+	int i=7;
+	printf("\t\t[func2“à] i@0x%o8x =%d\n",&i,i);
+	printf("\t\t[func2“à] j@0x%o8x =%d\n",&j,j);
+	printf("\t\t[func2“à]j‚É1337‚ğ‘ã“ü‚µ‚Ü‚·B\n");
+	j=1337;
+	func3();
+	printf("\t\t[func2‚É–ß‚Á‚Ä‚«‚Ü‚µ‚½] i@0x%o8x =%d\n",&i,i);
+	printf("\t\t[func2‚É–ß‚Á‚Ä‚«‚Ü‚µ‚½] j@0x%o8x =%d\n",&j,j);
+}
+void func1(){
+	int i=5;
+	printf("\t[func1“à] i@0x%o8x =%d\n",&i,i);
+	printf("\t[func1“à] j@0x%o8x =%d\n",&j,j);
+	func2();
+	printf("\t[func1‚É–ß‚Á‚Ä‚«‚Ü‚µ‚½] i@0x%o8x =%d\n",&i,i);
+	printf("\t[func1‚É–ß‚Á‚Ä‚«‚Ü‚µ‚½] j@0x%o8x =%d\n",&j,j);
+}
+int main(){
+	int i=3;
+	printf("[main“à] i@0x%o8x =%d\n",&i,i);
+	printf("[main“à] j@0x%o8x =%d\n",&j,j);
+	func1();
+	printf("[main‚É–ß‚Á‚Ä‚«‚Ü‚µ‚½] i@0x%o8x =%d\n",&i,i);
+	printf("[main‚É–ß‚Á‚Ä‚«‚Ü‚µ‚½] j@0x%o8x =%d\n",&j,j);
+	return 0;
+}
