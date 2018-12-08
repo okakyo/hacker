d9b28feb4a63c9ca8e99b9e91791908c8899065e
Kyohei Oka <0622okakyo@gmail.com>
2018-12-08 13:50:09 +0900

    '練習中'

---
+++ basic_c/typecasting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	float c,d;
+
+	a=13;b=5;
+	c=a/b;
+	d=(float)a/(float)b;
+	printf("[®”] a=%d\tb=%d\n%",a,b);
+	printf("[•‚“®¬”“_] c=%f\td=%f\n",c,d);
+
+return 0;}
