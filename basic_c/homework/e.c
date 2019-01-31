c39fd1b5f54ea5aaeb26557dc3ec6d95a4ca2714
Kyohei Oka <0622okakyo@gmail.com>
2019-01-31 15:10:12 +0900

    '修正完了'

--- basic_c/homework/e.c
+++ basic_c/homework/e.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 #include <math.h>
 
 #include "ps.h"
 
 
 int main(void){
-
 	init();
 	viewport(0.2, 0.2, 0.8,0.8);
 	xyworld(-1.0,-1.0, 1.0,1.0);
@@ -27,14 +25,18 @@ int main(void){
 	plot(0.2,0.1,2);
 	plot(0.8,0.1,2);
 	//Ç–Ç∞ÇÃà íuê›íË
-	for(int i=0;i<8;i++){
-		if(i==2 or i==6) continue;
-	th+=npi
-	X+=r*cos()+;
-	y+=i*sin()+; 
-	plot(,,3);
-	plot(,,2);
-	plot(,,2);
+	double r1=0.2,r2=0.6;
+	for(int i=0;i<12;i++){
+		if(i==5|| i==6|| i==7 ||i==11|| i==1 || i==0 ){
+	double th=2.0*PI*((double)i/12.0);
+	double x1=r1*cos(th)+0.5;
+	double y1=r1*sin(th)+0.3; 
+	double x2=r2*cos(th)+0.5;
+	double y2=r2*sin(th)+0.3; 
+	plot(x1,y1,3);
+	plot(x1,y1,2);
+	plot(x2,y2,2);
+		}
 	}
 
 	stroke();
